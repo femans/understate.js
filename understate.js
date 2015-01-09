e870287fc73de42db30e37bef532c07301376c28
f.emans <frido@emans.nl>
2015-01-09 23:30:28 +0100

    understate.js

---
+++ understate.js
@@ -0,0 +1,27 @@
+var keep_in_place = false;
+var understate = require('./understate.js');
+
+function testRunner(){
+    var counter = 1;
+    function test(string, exp){
+        return counter++ + ': '+(string==exp) +': '+string+ ' - '+exp;
+    }
+    var firstname='Homer', lastname='Simpson';
+    console.log(test('hello _!'._('world'), 'hello world!'));
+    console.log(test('my name is _ _'._(firstname, lastname), 'my name is '+firstname+' '+lastname));
+    console.log(test('this is _1_; _1_ is a _2_'._('bob', 'builder'), 'this is bob; bob is a builder'));
+    console.log(test('this is _name_ the _profession_'._({profession: 'builder', name: 'bob'}), 'this is bob the builder'));
+    console.log(test('_first_ _1_'._('expressions', {first: 'mixed'}), 'mixed expressions'));
+    console.log(test('_1_'._('which', {1:'one?'}), 'which'));
+    console.log(test('_'._('_named_', {named: 'hello world'}), 'hello world'));
+    console.log(test('hello /_ _'._('world'), 'hello _ world' ));
+
+    console.log(test('_3_'._(), ''));
+    console.log(test('_/__'._(), '/_'));
+    //keep in place test
+    keep_in_place = true;
+    console.log(test('_/__'._(), '_/__'));
+
+}
+testRunner();
+module.exports = understate;
