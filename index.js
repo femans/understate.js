c35d7887c47e34aada602bb4f8d91eafc389b141
f.emans <frido@emans.nl>
2015-01-10 03:13:04 +0100

    removed mixing patterns from feature list; fixed most things I think.

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 var understate = require('./understate.js');
 
-console.log("test understate.js:")
-
 function testRunner(){
     var counter = 1;
     function test(string, exp){
         var test = string==exp;
         return (test?"\033[37m":"\033[31m")+counter++ + ': '+test +': '+string+ ' - '+exp+"\033[37m";
     }
+
+    console.log("test understate.js:")
+
     var firstname='Homer', lastname='Simpson';
     console.log(test('hello _!'._('world'), 'hello world!'));
     console.log(test('my name is _ _'._(firstname, lastname), 'my name is '+firstname+' '+lastname));
-    console.log(test('this is _1_; _1_ is a _2_'._('bob', 'builder'), 'this is bob; bob is a builder'));
+    console.log(test('this is _1_; _1_ is a _2_'._('Bob', 'builder'), 'this is Bob; Bob is a builder'));
+    console.log(test('escaped: /_1/_; unescaped: _1_'._('this'), 'escaped: _1_; unescaped: this'));
     console.log(test('this is _name_ the _profession_'._({profession: 'builder', name: 'bob'}), 'this is bob the builder'));
-    console.log(test('_first_ _1_'._('expressions', {first: 'mixed'}), 'mixed expressions'));
-    console.log(test('_1_'._('which', {1:'one?'}), 'which'));
-    console.log(test('_'._('_named_', {named: 'hello world'}), 'hello world'));
+    console.log(test('/__2_ _'._('one?', 'which'), '_which one?'));
     console.log(test('hello /_ _'._('world'), 'hello _ world' ));
 
     console.log(test('_3_'._(), ''));
     console.log(test('_/__'._(), '_'));
+    understate.keep_in_place(true);
+    console.log(test('_3_'._(), '_3_'));
+    console.log(test('_/__'._(), '_/__'));
 }
 testRunner();
--- understate.js
+++ understate.js
@@ -1,42 +1,62 @@
 /*
+ * >> require('./understate')
+ * or: 
+ * >> require('./understate')(true)
+ *
  *   'hello _!'._('world') => 'hello world!'
  *   'my name is _ _'._(firstname, lastname) => 'my name is '+firstname+' '+lastname
  *   'this is _1_; _1_ is a _2_'._('bob', 'builder') => 'this is bob; bob is a builder'
  *   'this is _name_ the _profession_'._({profession: 'builder', name: 'bob'}) => 'this is bob the builder'
  *   '_first_ _1_'._('expressions', {first: 'mixed'}) => 'mixed expressions'
  *   '_1_'._('which', {1:'one?'}) => 'which'
- *   '_'._('_named_', {named: 'hello world'}) => 'hello world'
  *
  *   'hello /_ _'._('world') => 'hello _ world' // '/' escapes underscore for understate processor
  *
  * syntax: 
  *   string._(a,b,...) => replace underscores or numbered underscores by the arguments; 
  *   string._({a:x, b:y, ...}) object-arguments will attempt to replace named underscores with their values. 
- *   string._(a,b,{a:x,...}) => replace underscores or numbered underscores by the arguments; the last argument will attempt to replace named underscores by arguments in the string in a second pass  
- * Unmatched underscore sequences will be removed from the string by default
+ *
+ * * A '/' before an underscore escapes the underscore for the understate processor. 
+ * * Named underscore patterns are not allowed to have underscores, spaces or slashes in them.
+ * * Named patterns can be only numbers if not mixed with unnamed patterns.
+ * * Unmatched underscore sequences will be removed from the string by default; this behaviour can be changed by calling the module with for example 
+ * >> require('./understate')(true)
  */
 
-// version 2 plan: allow for different symbol as replacer; allow option to keep unmatched replacers in place
 
 /*
  * TODO:
- * 1. implement single pass, non-indexed replacers with escaping - done
- * 2. implement in the same pass numbered replacers with escaping - done
- * 3. implement named replacers, multi-pass
  * 4. implement alternative replacer 
- * 5. option to keep unmatched replacer in place = done
  */
 
 var keep_in_place = false;
-understate = function(keep){
-    if(keep)keep_in_place=true;
-}
-String.prototype._ = function(){
-    var args = arguments, 
-        position = -1;
-    var r = this.replace(/(\/)?_(\d+)(\/)?_/g, function(_, $0,$1,$2){return ($0||$2)?_:(args[$1-1]||(keep_in_place?_:''))})
-             .replace(/(\/)?(_)/g, function(match, slash, _){return slash?_:(args[++position]||(keep_in_place?_:''))});
-    return r;
+
+module.exports.keep_in_place = function(keep){
+    if(keep)keep_in_place=keep;
+    return this;
 }
 
-module.exports = understate;
+String.prototype._ = function(){
+    var args = Array.apply(null, arguments), 
+        named_args;
+
+    if(typeof args[0] == 'object')
+        named_args = args[0];
+
+    if(!named_args){
+        var re1 = new RegExp("(\\/)?_(\\d+)_", "gm"),
+            re2 = new RegExp("(\\/)?(_)", "gm"),
+            position = -1;
+                
+        return this.replace(re1, function(_, $0,$1){
+            return ($0)?_:(args[$1-1]||(keep_in_place?_:''))
+        })
+        .replace(re2, function(m, slash, _){
+            return slash?(keep_in_place?m:_):(args[++position]||(keep_in_place?_:''))
+        });
+    }
+    else
+        return this.replace(/(\/)?_([^/_\s]*)(\/)?_/gm, function(_, $0,$1,$2){
+            return ($0||$2)?_:(named_args[$1]||(keep_in_place?_:''))
+        });
+}
